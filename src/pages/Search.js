5d102d94b0a396c19f1bd7e377035257230e5c00
manu-unbxd <manu.sunny@unbxd.com>
2022-06-08 14:21:57 +0530

    3 files modified

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -6,13 +6,14 @@ import {
   useLocation,
 } from 'react-router-dom';
 import unbxdSearchConfig from '../config/siteconfig.json';
+import UnbxdSearch from '@unbxd-ui/vanilla-search-library';
 const unbxdCallbackEcma = function (instance, type, data) {};
 
 export default function Search() {
   let navigate = useNavigate();
   const { hash } = useLocation();
   const loadUnbxdSearch = () => {
-    window.unbxdSearch = new window.UnbxdSearch({
+    window.unbxdSearch = new UnbxdSearch({
       siteKey: unbxdSearchConfig.siteKey,
       apiKey: unbxdSearchConfig.apiKey,
       hashMode: true,
@@ -24,73 +25,28 @@ export default function Search() {
           navigate(`/product/${product.uniqueId}`);
         },
       },
+      facets: {
+        facetsEl: document.querySelectorAll('.UNX-facet-wrapper'),
+      },
     });
   };
   useEffect(() => {
-    if (window.UnbxdSearch) {
+    console.log(hash, 'hash');
+    if (
+      !window.unbxdSearch ||
+      window.unbxdSearch.getQueryParams().q !==
+        window.unbxdSearch.state.userInput
+    ) {
       loadUnbxdSearch();
     }
   }, [hash]);
   return (
     <div className="container-body">
       <div className="UNX-results-container">
-        <div className="UNX-head-wrapper">
-          <div className="UNX-selected-actions">
-            <div className="UNX-bread-wrapper" id="breadcrumpContainer"></div>
-            <div
-              className="UNX-selected-facet-wrapper"
-              id="selectedFacetWrapper"
-            ></div>
-          </div>
-          <div
-            className="UNX-product-type-block"
-            id="productViewTypeContainer"
-          ></div>
-        </div>
+        <div className="UNX-head-wrapper"></div>
         <div className="UNX-product-results">
-          <div className="UNX-facet-wrapper">
-            <div className="UNX-fxd-facet">
-              <div
-                className="UNX-selected-facet-wrapper UNX-selected-f-m"
-                id="selectedMFacetWrapper"
-              ></div>
-              <div
-                className="UNX-multilevel-block"
-                id="bucketedFacetWrapper"
-              ></div>
-              <div className="UNX-text-facet-block" id="facetsWrapper"></div>
-              <div className="UNX-m-facet-row">
-                <button
-                  data-action="applyFacets"
-                  className="UNX-primary-btn UNX-facet-trigger"
-                >
-                  Apply
-                </button>
-                <button
-                  data-action="clearFacets"
-                  className="UNX-default-btn UNX-facet-trigger"
-                >
-                  Clear
-                </button>
-              </div>
-            </div>
-          </div>
+          <div className="UNX-facet-wrapper"></div>
           <div className="UNX-product-list">
-            <div className="UNX-result-header">
-              <div id="didYouMeanWrapper" className="UNX-didyou-mean"></div>
-              <div className="UNX-result-right">
-                <div
-                  className="UNX-change-products"
-                  id="changeNoOfProducts"
-                ></div>
-                <div className="UNX-sort-wrapper" id="sortWrapper"></div>
-                <div
-                  className="UNX-change-pagination-wrap"
-                  id="paginationContainer"
-                ></div>
-              </div>
-            </div>
-            <div id="bannerContainer"></div>
             <div
               className="UNX-product-wrapper"
               id="searchResultsWrapper"
